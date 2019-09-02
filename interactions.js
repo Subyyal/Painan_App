a8597687a734420aa77dd24153f1621228172e67
subyyal khan <48442798+Subyyal@users.noreply.github.com>
2019-09-02 14:00:57 +0500

    day3 (js added)

--- interactions.js
+++ interactions.js
@@ -1,1 +1,18 @@
-console.log("CONNECTED");
+document.querySelector("#signinbutton").addEventListener("click",function () {
+
+    const e_input = document.querySelector("#emailinput").value;
+    const  p_input = document.querySelector("#pwinput").value;
+
+    if (e_input ==="subyyallk@gmail.com" && p_input=== "1234"){
+
+        window.location = "menu.html";
+
+
+    }
+    else {
+
+        alert("PLEASE TYPE CORRECT INFORMATION !!! ")
+
+    }
+
+});
